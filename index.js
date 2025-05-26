a1a6513e0a182ee8eb5eb72a263b671c4cb62033
Euclides-Gune <es7.comercial@gmail.com>
2025-05-26 18:37:17 +0200

    changing message settings

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function choice() {
     const title = event.currentTarget.parentNode.parentNode.parentNode.children[0].innerHTML;
     console.log(title);
     const a = event.currentTarget.parentNode.parentNode.children[0];
-    const message = `*Presente: *${title}%0A*remetente: *`;
+    const message = `Presente: ${title}%0AConfirmado?:`;
     a.href = `https://wa.me/258847189894?text=${message}`;
 }
